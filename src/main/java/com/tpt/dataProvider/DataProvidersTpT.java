a49c6af50f0091f159771490484fb9a355e3e1af
llewellynsji <llewellyn.thattazhi@sjinnovation.com>
2023-03-27 21:40:04 +0530

    Dataprovider Code optimization

--- src/main/java/com/tpt/dataProvider/DataProvidersTpT.java
+++ src/main/java/com/tpt/dataProvider/DataProvidersTpT.java
@@ -17,22 +17,12 @@ public class DataProvidersTpT {
 	NewExcelLibraryTpT obj = new NewExcelLibraryTpT();
 	
 	@DataProvider(name = "Credentials")
-	public Object[][] getCredentials() {
-		// Totals rows count
-		int rows = obj.getRowCount("Credentials");
-		// Total Columns
-		int column = obj.getColumnCount("Credentials");
-		int actRows = rows - 1;
-
-		Object[][] data = new Object[actRows][column];
-
-		for (int i = 0; i < actRows; i++) {
-			for (int j = 0; j < column; j++) {
-				data[i][j] = obj.getCellData("Credentials", j, i+2);
-			}
-		}
+	public Object[][] Credentials() throws IOException{
+		FileInputStream fis = new FileInputStream(dataPath + "TestDataTpT.xlsx");
+		Object[][] data = getAllData(fis, "Credentials");
 		return data;
 	}
+	
 	
 	DataFormatter formatter = new DataFormatter();
 	String dataPath = "src\\test\\resources\\TestData\\";
